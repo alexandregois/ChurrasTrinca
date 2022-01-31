1854512a8a65fde0d72594641c6edea0eb655979
Alexandre Gois de Oliveira <alexandregois.ti@gmail.com>
2022-01-31 18:32:17 -0300

    .
    .

---
+++ ChuurasTrinca/ChuurasTrinca/ViewModels/ViewCadastroChurrasco.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ChuurasTrinca.ViewModels
+{
+    public partial class ViewCadastroChurrasco : ContentPage
+    {
+        public ViewCadastroChurrasco()
+        {
+            InitializeComponent();
+        }
+    }
+}

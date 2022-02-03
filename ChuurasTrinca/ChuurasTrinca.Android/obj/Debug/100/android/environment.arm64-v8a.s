9dc577f51d50d4419fae60ef12a802e24105773b
Alexandre Gois de Oliveira <alexandregois.ti@gmail.com>
2022-02-03 06:32:29 -0300

    Cadastro Churrasco
    Cadastro Churrasco

--- ChuurasTrinca/ChuurasTrinca.Android/obj/Debug/100/android/environment.arm64-v8a.s
+++ ChuurasTrinca/ChuurasTrinca.Android/obj/Debug/100/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"46fac9e1-58ab-45dc-ae23-44aafb56ecf2"
+	.asciz	"380d2da4-5a74-4c88-9d1f-6605584c149d"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- ChuurasTrinca/ChuurasTrinca/ViewModels/ViewListarChurrascosViewModel.cs
+++ ChuurasTrinca/ChuurasTrinca/ViewModels/ViewListarChurrascosViewModel.cs
@@ -11,9 +11,38 @@ namespace ChuurasTrinca.ViewModels
     {
         private INavigationService _navigationService;
 
+        private string _descricao;
+        public string Descricao
+        {
+            get => _descricao;
+            set => SetProperty(ref _descricao, value);
+        }
+
+        private DateTime _data;
+        public DateTime Data
+        {
+            get => _data;
+            set => SetProperty(ref _data, value);
+        }
+
+        private decimal _valorPessoa;
+        public decimal ValorPessoa
+        {
+            get => _valorPessoa;
+            set => SetProperty(ref _valorPessoa, value);
+        }
+
+        private decimal _totalArrecadado;
+        public decimal TotalArrecadado
+        {
+            get => _totalArrecadado;
+            set => SetProperty(ref _totalArrecadado, value);
+        }
+
         public ViewListarChurrascosViewModel(INavigationService navigationService)
             : base(navigationService)
         {
+            _navigationService = navigationService;
 
         }
     }
